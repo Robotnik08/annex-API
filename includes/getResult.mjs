5c33ded0a8e1dd7fa3644fd1725a5f8b17325297
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-09-07 23:42:14 +0200

    final template

---
+++ includes/getResult.mjs
@@ -0,0 +1,10 @@
+/**
+ * @file getResult.mjs
+ * @description This file contains the template for the root function for the result.
+ * @module getResult
+ */
+
+// root function for the result
+export function getResponse (req, res, next) {
+    // get the result from the request and send it back to the client, implement in the future
+}
