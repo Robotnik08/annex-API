1222e0ab8990f8322f9c32397bc50ce8d2e620ec
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-09-12 16:27:10 +0200

    final API finsihed

--- includes/getResult.mjs
+++ includes/getResult.mjs
@@ -6,78 +6,76 @@
 
 import fetch from "node-fetch";
 
-// root function for the result
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjM3NzgwMDliNjM1ZDA2NTVkMTdjMmFlYTc2YzIzZiIsInN1YiI6IjY0Zjg3MjU5NWYyYjhkMDBjNDM1MWNjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B9_HCUeJyEZWDTNF31ZVhYdxTmeymyidiPPfyu50dVg";
+
 export class ResponseHandler {
-  handler(req, res, next) {
-    // get the result from the request and send it back to the client, implement in the future
-
-    const url = //api variables
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjM3NzgwMDliNjM1ZDA2NTVkMTdjMmFlYTc2YzIzZiIsInN1YiI6IjY0Zjg3MjU5NWYyYjhkMDBjNDM1MWNjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B9_HCUeJyEZWDTNF31ZVhYdxTmeymyidiPPfyu50dVg",
-      },
-    };
-
-    fetch(url, options) // fetch the data from the API
-      .then((res) => res.json())
-      .then((json) => {
-        info_main(json);
-      })
-      .catch((err) => console.error("error:" + err));
-
-    // send an test JSON object
-
-    function info_main(json_main) {
-      const url_details =
-        "https://api.themoviedb.org/3/movie/" +
-        json_main.results[0].id +
-        "?language=en-US";
-      const options_details = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjM3NzgwMDliNjM1ZDA2NTVkMTdjMmFlYTc2YzIzZiIsInN1YiI6IjY0Zjg3MjU5NWYyYjhkMDBjNDM1MWNjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B9_HCUeJyEZWDTNF31ZVhYdxTmeymyidiPPfyu50dVg",
-        },
-      };
-
-      fetch(url_details, options_details)
-        .then((res) => res.json())
-        .then((json) => info_details(json))
-        .catch((err) => console.error("error:" + err));
-
-      function info_details(json_details) {
-
-
-        const url_credit =
-          "https://api.themoviedb.org/3/movie/" + json_main.results[0].id + "/credits?language=en-US";
-        const options_credit = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjM3NzgwMDliNjM1ZDA2NTVkMTdjMmFlYTc2YzIzZiIsInN1YiI6IjY0Zjg3MjU5NWYyYjhkMDBjNDM1MWNjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.B9_HCUeJyEZWDTNF31ZVhYdxTmeymyidiPPfyu50dVg",
-          },
-        };
-
-        fetch(url_credit, options_credit)
-          .then((res) => res.json())
-          .then((json) => info_credit(json))
-          .catch((err) => console.error("error:" + err));
-
-        function info_credit(json_credit) {
-          let out;
-          for (let i = 0; i < json_credit.cast.length; i++) {
-            out += json_details;
-            out += "<br>";
-          }
-          res.send(out);
+    handler(req, res, next) {
+        // get the search query from the headers
+        let searchQuery = req.headers.request;
+        if (searchQuery === undefined) {
+            res.status(400).json({message: "No search query provided."});
+            return;
         }
-      }
+        try {
+            searchQuery = JSON.parse(searchQuery);
+        } catch (error) {
+            res.status(400).json({message: "Search query format is not JSON."});
+            return;
+        }
+        switch (searchQuery.type) {
+            case "movie":
+                if (searchQuery.id === undefined) {
+                    res.status(400).json({message: "No movie ID provided."});
+                    return;
+                } else if (typeof searchQuery.id !== "number") {
+                    res.status(400).json({message: "Movie ID must be a number."});
+                    return;
+                }
+                handleMovieRequest(req, res, next, searchQuery.id);
+                break;
+            case "showing":
+                break;
+            case "now_playing":
+                searchQuery.page ??= 1;
+                if (typeof searchQuery.page !== "number") {
+                    res.status(400).json({message: "Page number must be a number."});
+                    return;
+                }
+                handleNowPlayingRequest(req, res, next, searchQuery.page);
+                break;
+            default:
+                res.status(400).json({message: "Invalid search query type. Must be one of: movie, showing, now_playing."});
+        }
+        // send an test JSON object
+        function handleMovieRequest (req, res, next, id) {
+            // get the movie info from the movie database from the id
+            const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${API_KEY}`,
+                }
+            };
+            fetch(url, options).then((res) => res.json())
+            .then((json) => {
+                res.json(json);
+            });
+        }
+        function handleNowPlayingRequest (req, res, next, page) {
+            // get the movie info from the movie database from the id
+            const url = `https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${page}`;
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${API_KEY}`,
+                }
+            };
+            fetch(url, options).then((res) => res.json())
+            .then((json) => {
+                res.json(json);
+            });
+        }
     }
-  }
 }
