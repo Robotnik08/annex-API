aa0c2a3ad5fba955c781e9223f6c42e2289dca13
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-09-14 00:50:28 +0200

    fixed null error

--- db_seeder/seeder.php
+++ db_seeder/seeder.php
@@ -31,6 +31,7 @@
     $dbname = "annex-bios";
     
     //pdo
+    $conn = null;
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
